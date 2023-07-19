747a8b19270325e630882d6a7a498798b43e8665
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-19 22:08:22 +0530

    CustomerController created

--- src/main/java/com/sasstack/posadvance/api/CustomerController.java
+++ src/main/java/com/sasstack/posadvance/api/CustomerController.java
@@ -1,12 +1,36 @@
 package com.sasstack.posadvance.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/customer")
 public class CustomerController {
+
+    @PostMapping
+    public String createCustomer() {
+        return "Create Customer";
+    }
+
+    @PutMapping
+    public String updateCustomer() {
+        return "Update Customer";
+    }
+
+    @DeleteMapping
+    public String deleteCustomer() {
+        return "Delete Customer";
+    }
+
+    @GetMapping
+    public String findCustomer() {
+        return "Find Customer";
+    }
+
+    @GetMapping("/all")
+    public String getAllCustomers() {
+        return "Find All Customers";
+    }
+
 
 }
