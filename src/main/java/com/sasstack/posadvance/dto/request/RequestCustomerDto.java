2fd3586460ddb8a3658a6e9d0cb1cc8bdee2d03f
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-19 22:28:35 +0530

    RequestCustomerDto file configured

--- src/main/java/com/sasstack/posadvance/dto/request/RequestCustomerDto.java
+++ src/main/java/com/sasstack/posadvance/dto/request/RequestCustomerDto.java
@@ -1,4 +1,15 @@
 package com.sasstack.posadvance.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestCustomerDto {
+
+    private String name;
+    private String address;
+    private double salary;
 }
