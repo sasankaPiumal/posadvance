6c2b5608fc6205f0bc3b4cba5936b23e0a3e17d3
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-19 23:20:20 +0530

    SwaggerConfig updated.

--- src/main/java/com/sasstack/posadvance/config/SwaggerConfig.java
+++ src/main/java/com/sasstack/posadvance/config/SwaggerConfig.java
@@ -1,4 +1,20 @@
 package com.sasstack.posadvance.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public Docket SwaggerApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
