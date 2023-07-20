c1527a99c24cb7fca5080176414b345c9c4f306c
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-20 15:31:51 +0530

    abstract method added

--- src/main/java/com/sasstack/posadvance/service/CustomerService.java
+++ src/main/java/com/sasstack/posadvance/service/CustomerService.java
@@ -1,4 +1,18 @@
 package com.sasstack.posadvance.service;
 
+import com.sasstack.posadvance.dto.request.RequestCustomerDto;
+import com.sasstack.posadvance.dto.response.ResponseCustomerDto;
+import com.sasstack.posadvance.dto.response.paginated.model.CustomerPaginatedDto;
+
 public interface CustomerService {
+    public ResponseCustomerDto createCustomer(RequestCustomerDto dto);
+
+    public ResponseCustomerDto findCustomer(long id);
+
+    public ResponseCustomerDto updateCustomer(long id, RequestCustomerDto dto);
+
+    public void deleteCustomer(long id);
+
+    public CustomerPaginatedDto getAllCustomers(int page, int size, String searchText);
 }
+ 
