55c9473c2a341bc23f1282ad04c67389f7385a22
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-20 10:21:14 +0530

    FileResource.java class added to entity.process package

---
+++ src/main/java/com/sasstack/posadvance/entity/process/FileResource.java
@@ -0,0 +1,17 @@
+package com.sasstack.posadvance.entity.process;
+
+import java.sql.Blob;
+
+public class FileResource {
+
+    //=========================
+    private Blob fileName;
+
+    private Blob resourceUrl;
+
+    private Blob directory;
+
+    private Blob hash;
+    //=========================
+
+}
