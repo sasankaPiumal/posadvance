b6331ccade11bbe7e0140302e3ec8a435cf1fdd0
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-20 10:42:01 +0530

    CustomerRepo.java interface created.

--- src/main/java/com/sasstack/posadvance/repo/CustomerRepo.java
+++ src/main/java/com/sasstack/posadvance/repo/CustomerRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @EnableJpaRepositories
 public interface CustomerRepo extends JpaRepository<Customer,Long> {
