f605cd7311bcb2af08ecb99a07de586edda9d7d2
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-20 10:28:06 +0530

    FileResource embedded to Customer.java class

--- src/main/java/com/sasstack/posadvance/entity/process/FileResource.java
+++ src/main/java/com/sasstack/posadvance/entity/process/FileResource.java
@@ -1,17 +1,33 @@
 package com.sasstack.posadvance.entity.process;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.sql.Blob;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class FileResource {
 
-    //=========================
+    @Column(columnDefinition = "LONGBLOB")
     private Blob fileName;
 
+    @Column(columnDefinition = "LONGBLOB")
     private Blob resourceUrl;
 
+    @Column(columnDefinition = "LONGBLOB")
     private Blob directory;
 
+    @Column(columnDefinition = "LONGBLOB")
     private Blob hash;
-    //=========================
+
 
 }
