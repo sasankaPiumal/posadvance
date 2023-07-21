17130889c1f3af2a53d0fa200a915d0a876f5f5f
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-07-21 07:09:47 +0530

    updated

--- src/main/java/com/sasstack/posadvance/repo/CustomerRepo.java
+++ src/main/java/com/sasstack/posadvance/repo/CustomerRepo.java
@@ -2,13 +2,19 @@ package com.sasstack.posadvance.repo;
 
 import com.sasstack.posadvance.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 @EnableJpaRepositories
-public interface CustomerRepo extends JpaRepository<Customer,Long> {
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
+    //    @Modifying
+    @Query(value = "SELECT * FROM customer WHERE public_id=?1", nativeQuery = true)
+    public Optional<Customer> findByPublicId(Long id);
+
+    public void deleteByPublicIdIs(long id);
 }
