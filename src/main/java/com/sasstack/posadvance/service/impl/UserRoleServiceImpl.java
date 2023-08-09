ca71dfed4bad45346ba68c78510e9a8327df8f1e
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 21:30:55 +0530

    spring security configure 8 - UserRoleService service implemented.

---
+++ src/main/java/com/sasstack/posadvance/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sasstack.posadvance.service.impl;
+
+import com.sasstack.posadvance.service.UserRoleService;
+
+public class UserRoleServiceImpl implements UserRoleService {
+    @Override
+    public void initializeRoles() {
+
+    }
+}
