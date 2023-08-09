9d55f87a5ae24d422ca90ba9ff7a7ae9f4528055
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 23:40:02 +0530

    updated .

--- src/main/java/com/sasstack/posadvance/api/UserController.java
+++ src/main/java/com/sasstack/posadvance/api/UserController.java
@@ -1,4 +1,23 @@
 package com.sasstack.posadvance.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    private UserService userService;
+
+    public ResponseEntity<StackOverflowError> createUser(){
+
+
+    }
+
+
+
+
 }
