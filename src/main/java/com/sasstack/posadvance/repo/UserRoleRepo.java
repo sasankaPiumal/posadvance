2a531105198a937e88f056e10740c61a64dffb4c
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 21:27:09 +0530

    spring security configure 8 - UserRoleRepo repository created

---
+++ src/main/java/com/sasstack/posadvance/repo/UserRoleRepo.java
@@ -0,0 +1,13 @@
+package com.sasstack.posadvance.repo;
+
+import com.sasstack.posadvance.entity.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRoleRepo extends JpaRepository<UserRole,String> {
+
+    public void initializeReles();
+}
