504b5e374950fbe62785cd8f4328fc38e5bde8a0
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 22:28:16 +0530

    updated with fields.

--- src/main/java/com/sasstack/posadvance/dto/core/UserDto.java
+++ src/main/java/com/sasstack/posadvance/dto/core/UserDto.java
@@ -1,4 +1,17 @@
 package com.sasstack.posadvance.dto.core;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+    private String userId;
+    private String email;
+    private String fullName;
+    private String password;
 }
