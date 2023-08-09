64c68d603551051e2d0b5562fee4737f4583dac9
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-10 00:38:19 +0530

    updated.
    .

--- src/main/java/com/sasstack/posadvance/service/impl/UserServiceImpl.java
+++ src/main/java/com/sasstack/posadvance/service/impl/UserServiceImpl.java
@@ -1,8 +1,9 @@
 package com.sasstack.posadvance.service.impl;
 
+import com.sasstack.posadvance.dto.core.UserDto;
 import com.sasstack.posadvance.dto.request.RequestUserDto;
 import com.sasstack.posadvance.entity.User;
-import com.sasstack.posadvance.entity.UserRole;
+import com.sasstack.posadvance.exception.DuplicateEntryException;
 import com.sasstack.posadvance.exception.EntryNotFoundException;
 import com.sasstack.posadvance.repo.UserRepo;
 import com.sasstack.posadvance.repo.UserRoleRepo;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -29,15 +31,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void createUser(RequestUserDto requestUserDto, String role) {
-       var selectedUserRole = userRoleRepo.findByRoleNameEquals(role);
-       if (selectedUserRole.isEmpty()){
-           throw new EntryNotFoundException("User not found!");
-       }{
+        var selectedUserRole = userRoleRepo.findByRoleNameEquals(role);
+        if (selectedUserRole.isEmpty()) {
+            throw new EntryNotFoundException("User not found!");
+        }
+        {
 
             Optional<User> userByEmail = userRepo.findUserByEmail(requestUserDto.getEmail());
-            if (userByEmail.isPresent()){
-
+            if (userByEmail.isPresent()) {
+                throw new DuplicateEntryException("User Email Already Exists!");
             }
+            UserDto dto = new UserDto(
+                    String.valueOf(new Random().nextInt(1000)),
+                    requestUserDto.getEmail(),
+                    requestUserDto.getFullName(),
+                    requestUserDto.getPassword());
         }
 
     }
