0531c9fdb014b3e496e680e9912328c4468150f3
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 21:19:07 +0530

    spring security configure 8 - deleted

---
+++ src/main/java/com/sasstack/posadvance/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.sasstack.posadvance.repo;
+
+import com.sasstack.posadvance.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepo extends JpaRepository<User,String> {
+}
