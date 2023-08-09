e8024db1132b61f0a93fc35e29b33df17f3e22c9
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-10 00:36:56 +0530

    DuplicateEntryException manager created
    .

--- src/main/java/com/sasstack/posadvance/adviser/AppWiderExceptionHandler.java
+++ src/main/java/com/sasstack/posadvance/adviser/AppWiderExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sasstack.posadvance.adviser;
 
+import com.sasstack.posadvance.exception.DuplicateEntryException;
 import com.sasstack.posadvance.exception.EntryNotFoundException;
 import com.sasstack.posadvance.util.StandardResponse;
 import org.springframework.http.HttpStatus;
@@ -11,19 +12,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppWiderExceptionHandler {
 
     @ExceptionHandler(EntryNotFoundException.class)
-    public ResponseEntity<StandardResponse> handleEntryNotFoundException(EntryNotFoundException e){
+    public ResponseEntity<StandardResponse> handleEntryNotFoundException(EntryNotFoundException e) {
         return new ResponseEntity<StandardResponse>(
-                new StandardResponse(404,e.getMessage(),e), HttpStatus.NOT_FOUND
+                new StandardResponse(404, e.getMessage(), e), HttpStatus.NOT_FOUND
         );
     }
 
     @ExceptionHandler(ClassNotFoundException.class)
-    public ResponseEntity<StandardResponse> handleClassNotFoundException(ClassNotFoundException exception){
+    public ResponseEntity<StandardResponse> handleClassNotFoundException(ClassNotFoundException exception) {
         return new ResponseEntity<StandardResponse>(
-                new StandardResponse(404, exception.getMessage(),exception),HttpStatus.NOT_FOUND
-
+                new StandardResponse(404, exception.getMessage(), exception), HttpStatus.NOT_FOUND
         );
+    }
 
+    @ExceptionHandler(DuplicateEntryException.class)
+    public ResponseEntity<StandardResponse> handleDuplicateEntryException(DuplicateEntryException e) {
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse(409, e.getMessage(), e), HttpStatus.CONFLICT
+        );
     }
 
 }
