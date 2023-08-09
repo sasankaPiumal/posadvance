fad369536435ef0cefd15775ed8fe6614aec7092
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-10 00:17:29 +0530

    exception class updated .
    .

--- src/main/java/com/sasstack/posadvance/exception/DuplicateEntryException.java
+++ src/main/java/com/sasstack/posadvance/exception/DuplicateEntryException.java
@@ -1,4 +1,29 @@
 package com.sasstack.posadvance.exception;
 
-public class DuplicateEntryException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class DuplicateEntryException extends RuntimeException{
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
+    public DuplicateEntryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateEntryException(String msg){
+        super(msg);
+    }
 }
