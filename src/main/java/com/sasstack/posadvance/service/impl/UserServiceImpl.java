5e8ed2793fc0269df4ebc2e5384f486bf17dba71
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 23:56:02 +0530

    updated.
    .

--- src/main/java/com/sasstack/posadvance/service/impl/UserServiceImpl.java
+++ src/main/java/com/sasstack/posadvance/service/impl/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package com.sasstack.posadvance.service.impl;
 
-public class UserServiceImpl {
+import com.sasstack.posadvance.dto.request.RequestUserDto;
+import com.sasstack.posadvance.repo.UserRepo;
+import com.sasstack.posadvance.repo.UserRoleRepo;
+import com.sasstack.posadvance.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+
+    private final UserRoleRepo userRoleRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepo userRepo, UserRoleRepo userRoleRepo) {
+        this.userRepo = userRepo;
+        this.userRoleRepo = userRoleRepo;
+    }
+
+    @Override
+    public void createUser(RequestUserDto requestUserDto, String role) {
+
+    }
+
+
 }
