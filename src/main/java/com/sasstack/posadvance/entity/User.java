b276e5ad2532a01919d4bced297770bfbc1280fd
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 19:00:06 +0530

    spring security configure 2 - user entity created.

---
+++ src/main/java/com/sasstack/posadvance/entity/User.java
@@ -0,0 +1,49 @@
+package com.sasstack.posadvance.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "user_id",length = 45)
+    private String userId;
+
+    @Column(name = "email",length = 100)
+    private String email;
+
+    @Column(name = "full_name",length = 250)
+    private String fullName;
+
+    @Column(name = "password",length = 750)
+    private String password;
+
+    @Column(name = "is_account_non_expired",columnDefinition = "TINYINT")
+    private boolean isAccountNonExpired;
+
+    @Column(name = "is_account_non_locked",columnDefinition = "TINYINT")
+    private boolean isAccountNonLocked;
+
+    @Column(name = "is_credential_non_expired",columnDefinition = "TINYINT")
+    private boolean isCredentialNonExpired;
+
+    @Column(name = "is_enabled",columnDefinition = "TINYINT")
+    private boolean isEnabled;
+
+
+
+
+
+
+}
