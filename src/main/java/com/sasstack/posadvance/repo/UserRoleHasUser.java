8690858fbf213842c2bf9e1b2645130ef50a9194
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-10 01:08:05 +0530

    updated.

--- src/main/java/com/sasstack/posadvance/repo/UserRoleHasUser.java
+++ src/main/java/com/sasstack/posadvance/repo/UserRoleHasUser.java
@@ -1,4 +1,11 @@
 package com.sasstack.posadvance.repo;
 
-public interface UserRoleHasUser {
+import com.sasstack.posadvance.entity.UserRoleHasUserKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRoleHasUser extends JpaRepository<UserRoleHasUser,String> {
 }
