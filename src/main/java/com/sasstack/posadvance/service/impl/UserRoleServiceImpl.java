9917f27ed0d1099382f412b0f35eecb4f1ce4761
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 21:34:22 +0530

    spring security configure 9 - UserRoleServiceImpl updated with constructor-DI.

--- src/main/java/com/sasstack/posadvance/service/impl/UserRoleServiceImpl.java
+++ src/main/java/com/sasstack/posadvance/service/impl/UserRoleServiceImpl.java
@@ -1,8 +1,21 @@
 package com.sasstack.posadvance.service.impl;
 
+import com.sasstack.posadvance.repo.UserRoleRepo;
 import com.sasstack.posadvance.service.UserRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserRoleServiceImpl implements UserRoleService {
+
+    private final UserRoleRepo userRoleRepo;
+
+    @Autowired
+    public UserRoleServiceImpl(UserRoleRepo userRoleRepo) {
+        this.userRoleRepo = userRoleRepo;
+    }
+
+
     @Override
     public void initializeRoles() {
 
