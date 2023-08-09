8a212e12c5debfaaf7ed1dfa15a20e2f31a209f3
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-09 22:25:00 +0530

    created.

--- src/main/java/com/sasstack/posadvance/dto/request/RequestUserDto.java
+++ src/main/java/com/sasstack/posadvance/dto/request/RequestUserDto.java
@@ -1,4 +1,18 @@
 package com.sasstack.posadvance.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestUserDto {
+    private String email;
+    private String fullName;
+    private String password;
 }
