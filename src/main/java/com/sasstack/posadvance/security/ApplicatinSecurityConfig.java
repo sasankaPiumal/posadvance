21cceb6c60140d1b10e8aabddde7efb23360d733
sasanka <Sasanka@DESKTOP-84NE784.(none)>
2023-08-10 10:10:50 +0530

    configure(HttpSecurity http) method added.

--- src/main/java/com/sasstack/posadvance/security/ApplicatinSecurityConfig.java
+++ src/main/java/com/sasstack/posadvance/security/ApplicatinSecurityConfig.java
@@ -1,4 +1,16 @@
 package com.sasstack.posadvance.security;
 
-public class ApplicatinSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicatinSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+    }
 }
